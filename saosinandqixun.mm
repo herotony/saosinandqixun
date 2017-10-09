24f2e30955c3caf16c9a678f2cb437014e265597
herotony <herotony@gmail.com>
2017-10-09 14:35:39 +0800

    从即日起，必须通过freemind来组织文档整理，不能以atom来整理，那玩意还是乱，不易搜索

--- saosinandqixun.mm
+++ saosinandqixun.mm
@@ -31,10 +31,6 @@
 <icon BUILTIN="penguin"/>
 <node BACKGROUND_COLOR="#ccffcc" COLOR="#3333ff" CREATED="1503469264140" ID="ID_276678588" LINK="mdfiles/centos/NiceTool.markdown" MODIFIED="1504770362966" TEXT="&#x5e38;&#x7528;&#x5de5;&#x5177;">
 <font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="15"/>
-<node BACKGROUND_COLOR="#ffcc66" COLOR="#009933" CREATED="1503905748917" ID="ID_655266765" LINK="http://www.openvim.com/" MODIFIED="1503905807201" TEXT="&#x4e0d;&#x9519;&#x7684;vim&#x4eff;&#x771f;&#x7ec3;&#x4e60;&#x7f51;&#x7ad9;">
-<font BOLD="true" NAME="&#x5fae;&#x8f6f;&#x96c5;&#x9ed1;" SIZE="15"/>
-<icon BUILTIN="bookmark"/>
-</node>
 <node BACKGROUND_COLOR="#ccccff" COLOR="#009933" CREATED="1503916188551" ID="ID_886420423" LINK="mdfiles/centos/vim.markdown" MODIFIED="1504225652970" TEXT="vim&#x4e91;&#x914d;&#x7f6e;">
 <icon BUILTIN="idea"/>
 </node>
